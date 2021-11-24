fb5d9deefd50c9fc53279fb3c4b646e094b30318
j-mq <62872683+j-mq@users.noreply.github.com>
2021-11-24 12:18:34 +0900

    ThreePanel修正
    ThreePanel修正

--- src/components/FDS/Layouts/ThreePanel/ThreePanel.jsx
+++ src/components/FDS/Layouts/ThreePanel/ThreePanel.jsx
@@ -2,19 +2,12 @@ import React from 'react';
 import PropTypes from 'prop-types';
 
 const ThreePanel = (props) => {
-  const some = 'o';
   return (
-    <div className="fds-three-panel">
-      <div className="fds-three-panel__top">
-        {props.top}
-      </div>
-      <div className="fds-three-panel__bottom">
-        <div className="fds-three-panel__side-nav">
-          {props.leftNavi}
-        </div>
-        <div className="fds-three-panel__main">
-          {props.main}
-        </div>
+    <div className='fds-three-panel'>
+      <div className='fds-three-panel__top'>{props.top}</div>
+      <div className='fds-three-panel__bottom'>
+        <div className='fds-three-panel__side-nav'>{props.leftNavi}</div>
+        <div className='fds-three-panel__main'>{props.main}</div>
       </div>
     </div>
   );
